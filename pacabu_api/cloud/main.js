de3262552f213cb02042e84a380817eaa14bb910
zoosjuice <dkdejesus@gmail.com>
2013-06-01 21:21:26 -0700

    Changing main.js to deploy to cloud code

--- pacabu_api/cloud/main.js
+++ pacabu_api/cloud/main.js
@@ -1,6 +1,19 @@
+var twilio = require("twilio");
+twilio.initialize("AC0ed0cb4e469cf4c561c79c02056e36a3","022384045cbc5882ef8218c1dafcc879");
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.define('inviteWithTwilio', function(request, response) {
+    twilio.sendSMS({
+      From: "+12064960313",
+      To: request.params.number,
+      Body: "Start using Parse and Twilio! Your Verification number is 2386"
+    }, {
+    success: function(httpResponse) {
+      console.log(httpResponse);
+      response.success("SMS sent!");
+    },
+    error: function(httpResponse) {
+      console.error(httpResponse);
+      response.error("Uh oh, something went wrong");
+    }
+  });
 });
